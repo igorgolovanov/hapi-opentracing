b1ad77a7043e577a923bdba3df5b94b38cff6d4f
Igor Golovanov <igor.golovanov@gmail.com>
2019-04-04 17:46:01 +0300

    base

---
+++ test/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Code = require('code');
+const Lab = require('lab');
+const Hapi = require('hapi');
+const OpenTracing = require('..');
+
+const { describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+describe('OpenTracing', () => {
+
+});
